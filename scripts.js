a9cff655b3dfdbcb63648f0b51e640f433b40a4c
santiagobedoa <santiagobedoa@gmail.com>
2023-01-29 15:50:22 -0500

    Task 2: Videos Carousel completed

--- scripts.js
+++ scripts.js
@@ -3,10 +3,10 @@ $(document).ready(function () {
     url: "https://smileschool-api.hbtn.info/quotes",
     type: "get",
     beforeSend: function () {
-      $("#loaderDiv").show();
+      $("#QuotesLoader").show();
     },
     success: function (response) {
-      $("#loaderDiv").hide();
+      $("#QuotesLoader").hide();
       for (let i = 0; i < response.length; i++) {
         let $html = $(`
           <div class="carousel-item carousel-item-content ${
@@ -28,6 +28,55 @@ $(document).ready(function () {
               </div>
           </div>`);
         $("#quotesCarouselInner").append($html);
+      }
+    },
+  });
+
+  $.ajax({
+    url: "https://smileschool-api.hbtn.info/popular-tutorials",
+    type: "get",
+    beforeSend: function () {
+      $("#VideosLoader").show();
+    },
+    success: function (response) {
+      $("#VideosLoader").hide();
+      for (let i = 0; i < response.length; i++) {
+        let $stars = "";
+        for (let j = 0; j < response[i].star; j++) {
+          $stars +=
+            '<img src="./images/star_on.png" class="mr-1 carousel-star-icon" alt="star icon filled in purple">';
+        }
+        for (let j = 0; j < 5 - response[i].star; j++) {
+          $stars +=
+            '<img src="./images/star_off.png" class="carousel-star-icon" alt="star icon filled in grey">';
+        }
+        let $html = $(`
+              <div class="text-center col-12 col-sm-6 col-md-3">
+                  <div class="carousel-item active">
+                      <img class="w-100" src="${response[i].thumb_url}" alt="smile image">
+                      <div class="mx-3">
+                          <div class="font-weight-bold text-dark text-left mt-3">
+                              ${response[i].title}
+                          </div>
+                          <div class="text-secondary text-left mt-3 mb-3">
+                              ${response[i]["sub-title"]}
+                          </div>
+                          <div class="d-flex align-items-center mb-3">
+                              <img src="${response[i].author_pic_url}" class="rounded-circle mr-3 video-carousel-img-profile" alt="profile image">
+                              <div class="purple-text font-weight-bold">${response[i].author}</div>
+                          </div>
+                          <div class="d-flex justify-content-between">
+                              <div class="d-flex pt-1">
+                              ${$stars}
+                              </div>
+                              <div class="purple-text font-weight-bold">
+                                  ${response[i].duration}
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>`);
+        $("#tutorialvideos").append($html);
       }
     },
   });
